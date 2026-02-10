eac9f26520188140524da1381c5bc27279691b25
emeka <emekaowo@yahoo.com>
2026-02-10 12:25:46 +0100

    fix

--- components/features/AIChatButton.tsx
+++ components/features/AIChatButton.tsx
@@ -128,7 +128,7 @@ export default function AIChatButton() {
         </Button>
       </motion.div>
 
-      {/* Chat Window */}ich
+      {/* Chat Window */}ichfix
       <AnimatePresence>
         {isOpen && (
           <>
